d853927a8709533f306e39b8e17b42d84410b2a1
Cory <csturco0331@yahoo.com>
2016-11-09 14:21:36 -0600

    Practice

--- src/app.js
+++ src/app.js
@@ -5,9 +5,37 @@ import 'normalize.css'
 import './styles'
 
 $(document).ready(() => {
-  $('#app')
-    .html('hey')
-    .click(() => console.log('lol'))
-    .click()
-    throw new TypeError()
+  // $('#app')
+  //   .html('hey')
+  //   .click(() => console.log('lol'))
+  //   .click()
+  //   throw new TypeError()
+
+// let changeColor = (y) => {
+//   var classNames = $(y).attr('class').split(' ')
+//   if (classNames.some(x => x === 'red')) return 'red green'
+//   if (classNames.some(x => x === 'green')) return 'green blue'
+//   if (classNames.some(x => x === 'blue')) return 'blue red'
+// }
+//
+//   $('#first')
+//     .click(function () {$('#first').toggleClass(changeColor(this))})
+//
+//   $('#second')
+//     .click(function() {$('#second').toggleClass(changeColor(this))})
+//
+//   $('#third')
+//     .click(function() {$('#third').toggleClass(changeColor(this))})
+
+  let colorChange = function(color) {
+    if (color === 'rgb(255, 0, 0)') return 'green'
+    if (color === 'rgb(0, 128, 0)') return 'blue'
+    return 'red'
+  }
+
+  $('.colorChanger')
+    .click(function() {
+      let color = $(this).css('background-color')
+      $(this).css('background-color', colorChange(color))
+    })
 })
